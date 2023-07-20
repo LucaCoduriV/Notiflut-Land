1be2c0da6c4982092d7c15f9520f1fa29944e27d
Luca Coduri <coduri.vitor@gmail.com>
2023-07-20 15:57:32 +0200

    Ctl: Self reference with Rc

--- notiflut_ctl/src/dbus_client.rs
+++ notiflut_ctl/src/dbus_client.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use dbus::blocking::Connection;
 use dbus::blocking::Proxy;
+use std::rc::Rc;
 use std::time::Duration;
 
 /// D-Bus interface for desktop notifications.
@@ -9,51 +10,48 @@ pub const NOTIFICATION_INTERFACE: &str = "org.freedesktop.Notifications";
 /// D-Bus path for desktop notifications.
 pub const NOTIFICATION_PATH: &str = "/org/freedesktop/Notifications";
 
-pub struct DbusClient {
-    connection: Connection,
+pub struct DbusClient<'a> {
+    connection: Rc<Connection>,
+    proxy: Proxy<'a, Rc<Connection>>,
 }
 
-impl DbusClient {
+impl<'a> DbusClient<'a> {
     pub fn init() -> Result<Self> {
         let connection = Connection::new_session()?;
-        Ok(Self { connection })
+        let ref_counter = Rc::new(connection);
+        let dc: DbusClient = Self {
+            connection: ref_counter.clone(),
+            proxy: Proxy::new(
+                NOTIFICATION_INTERFACE,
+                format!("{NOTIFICATION_PATH}/ctl"),
+                Duration::from_millis(1000),
+                ref_counter,
+            ),
+        };
+
+        Ok(dc)
     }
 
     pub fn show_nc(&self) -> Result<String> {
-        let proxy = Proxy::new(
-            NOTIFICATION_INTERFACE,
-            format!("{NOTIFICATION_PATH}/ctl"),
-            Duration::from_millis(1000),
-            &self.connection,
-        );
-
-        let result: (String,) = proxy.method_call(NOTIFICATION_INTERFACE, "OpenNC", ())?;
+        let result: (String,) = self
+            .proxy
+            .method_call(NOTIFICATION_INTERFACE, "OpenNC", ())?;
 
         Ok(result.0)
     }
 
     pub fn toggle_nc(&self) -> Result<String> {
-        let proxy = Proxy::new(
-            NOTIFICATION_INTERFACE,
-            format!("{NOTIFICATION_PATH}/ctl"),
-            Duration::from_millis(1000),
-            &self.connection,
-        );
-
-        let result: (String,) = proxy.method_call(NOTIFICATION_INTERFACE, "ToggleNC", ())?;
+        let result: (String,) = self
+            .proxy
+            .method_call(NOTIFICATION_INTERFACE, "ToggleNC", ())?;
 
         Ok(result.0)
     }
 
     pub fn hide_nc(&self) -> Result<String> {
-        let proxy = Proxy::new(
-            NOTIFICATION_INTERFACE,
-            format!("{NOTIFICATION_PATH}/ctl"),
-            Duration::from_millis(1000),
-            &self.connection,
-        );
-
-        let result: (String,) = proxy.method_call(NOTIFICATION_INTERFACE, "CloseNC", ())?;
+        let result: (String,) = self
+            .proxy
+            .method_call(NOTIFICATION_INTERFACE, "CloseNC", ())?;
 
         Ok(result.0)
     }
