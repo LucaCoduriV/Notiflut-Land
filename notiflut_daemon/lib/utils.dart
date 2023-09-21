5a318b01dc32172a87bbbf4ca4eff7632ae0d04f
Luca Coduri <coduri.vitor@gmail.com>
2023-09-21 04:37:11 +0200

    refactor

--- notiflut_daemon/lib/utils.dart
+++ notiflut_daemon/lib/utils.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
+import 'package:notiflutland/messages/daemon_event.pb.dart' as daemon_event;
 
-/// Returns an Image from raw data 
+import 'messages/daemon_event.pbenum.dart';
+
+/// Returns an Image from raw data
 Image createImageIiibiiay(
     int width, int height, Uint8List bytes, int channels, int rowStride) {
   final image = img.Image.fromBytes(
@@ -20,6 +24,24 @@ Image createImageIiibiiay(
     height: 100,
     width: 100,
   );
+}
+
+ImageProvider? imageRawToProvider(daemon_event.ImageSource source) {
+  return switch (source.type.value) {
+    final value when value == ImageSource_ImageSourceType.Data.value =>
+      createImageIiibiiay(
+        source.imageData.width,
+        source.imageData.height,
+        Uint8List.fromList(source.imageData.data),
+        source.imageData.onePointTwoBitAlpha ? 4 : 3,
+        source.imageData.rowstride,
+      ).image,
+    final value
+        when value == ImageSource_ImageSourceType.Path.value &&
+            source.path.isNotEmpty =>
+      Image.file(File(source.path)).image,
+    _ => null,
+  };
 }
 
 extension StringExtension on String {
--- notiflut_daemon/lib/widgets/notification_list.dart
+++ notiflut_daemon/lib/widgets/notification_list.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:get_it_mixin/get_it_mixin.dart';
-import 'package:notiflutland/messages/daemon_event.pbenum.dart';
 import 'package:notiflutland/services/notification_service.dart';
 import 'package:notiflutland/utils.dart';
 import 'package:notiflutland/widgets/notification.dart';
@@ -17,29 +13,8 @@ class NotificationList extends StatelessWidget with GetItMixin {
         watchOnly((NotificationService service) => service.notifications);
     return ListView(
       children: notifications.map((e) {
-        ImageProvider<Object>? imageProvider = switch(e.appImage.type.value){
-          final value when value == ImageSource_ImageSourceType.Data.value => createImageIiibiiay(
-              e.appImage.imageData.width,
-              e.appImage.imageData.height,
-              Uint8List.fromList(e.appImage.imageData.data),
-              e.appImage.imageData.onePointTwoBitAlpha ? 4 : 3,
-              e.appImage.imageData.rowstride,
-            ).image,
-          final value when value == ImageSource_ImageSourceType.Path.value && e.appImage.path.isNotEmpty => Image.file(File(e.appImage.path)).image,
-          _ => null,
-        };
-
-        ImageProvider<Object>? iconeProvider = switch(e.appIcon.type.value){
-          final value when value == ImageSource_ImageSourceType.Data.value => createImageIiibiiay(
-              e.appIcon.imageData.width,
-              e.appIcon.imageData.height,
-              Uint8List.fromList(e.appImage.imageData.data),
-              e.appIcon.imageData.onePointTwoBitAlpha ? 4 : 3,
-              e.appIcon.imageData.rowstride,
-            ).image,
-          final value when value == ImageSource_ImageSourceType.Path.value && e.appIcon.path.isNotEmpty => Image.file(File(e.appIcon.path)).image,
-          _ => null,
-        };
+        ImageProvider<Object>? imageProvider = imageRawToProvider(e.appImage);
+        ImageProvider<Object>? iconeProvider = imageRawToProvider(e.appIcon);
         return NotificationTile(
           e.id,
           e.appName,
